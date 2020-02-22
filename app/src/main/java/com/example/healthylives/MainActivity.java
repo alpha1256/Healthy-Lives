f0ae5235f53e802201e441c87ae935063db6d1cc
Martin Gryder <mgryd001@odu.edu>
2020-02-22 14:26:03 -0500

    menu prototype

--- app/src/main/java/com/example/healthylives/MainActivity.java
+++ app/src/main/java/com/example/healthylives/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.healthylives;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
