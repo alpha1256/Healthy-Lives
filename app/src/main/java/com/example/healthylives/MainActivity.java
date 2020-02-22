4e1e089d4e6f0a6e087e8ac5331bdea07b992d8e
gdin <alphadeegee@gmail.com>
2020-02-22 14:58:47 -0500

    menu items updated and on click takes user to corresponding activity

--- app/src/main/java/com/example/healthylives/MainActivity.java
+++ app/src/main/java/com/example/healthylives/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.healthylives;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
+
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,47 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.hr)
+        {
+            Intent intent = new Intent(this, heartRateActivity.class);
+            startActivity(intent);
+            return false;
+        }
+
+        else if (id == R.id.workout)
+        {
+            Intent intent = new Intent(this, workoutActivity.class);
+            startActivity(intent);
+            return false;
+        }
+
+        else if (id == R.id.sleep)
+        {
+            Intent intent = new Intent(this, sleepActivity.class);
+            startActivity(intent);
+            return false;
+        }
+
+        else if (id == R.id.social)
+        {
+            Intent intent = new Intent(this, socialActivity.class);
+            startActivity(intent);
+            return false;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean callIntent(Activity name)
+    {
+        Intent intent = new Intent(this, name.getClass());
+        startActivity(intent);
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/healthylives/sleepActivity.java
@@ -0,0 +1,14 @@
+package com.example.healthylives;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sleepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep);
+    }
+}
