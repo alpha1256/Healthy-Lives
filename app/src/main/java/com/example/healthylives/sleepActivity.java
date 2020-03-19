739f18fe1b93a1e034b7f47818828e1956261c53
Martin Gryder <mgryd001@odu.edu>
2020-03-19 14:11:37 -0400

    added gui for sleep activity

--- app/src/main/java/com/example/healthylives/sleepActivity.java
+++ app/src/main/java/com/example/healthylives/sleepActivity.java
@@ -3,13 +3,28 @@ package com.example.healthylives;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class sleepActivity extends AppCompatActivity {
+
+    TextView recordSleep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
         setTitle("Sleep");
+        recordSleep = (TextView) findViewById(R.id.recordSleep);
+    }
+
+    public void onClickStart(View v)
+    {
+        recordSleep.setText(String.valueOf("recording"));
+    }
+
+    public void onClickStop (View v)
+    {
+        recordSleep.setText(" ");
     }
 }
