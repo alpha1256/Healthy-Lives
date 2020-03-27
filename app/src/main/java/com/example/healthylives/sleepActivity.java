6d1cebc32432ad0777c3dcb5bb51eff77fb4a392
Martin Gryder <mgryd001@odu.edu>
2020-03-27 00:46:06 -0400

    Finally got sleep activity to run in the background

--- app/src/main/java/com/example/healthylives/sleepActivity.java
+++ app/src/main/java/com/example/healthylives/sleepActivity.java
@@ -2,19 +2,21 @@ package com.example.healthylives;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 import static com.example.healthylives.MainActivity.sleepMin;
+import static com.example.healthylives.SleepTimer.time;
 
 public class sleepActivity extends AppCompatActivity {
 
     TextView recordSleep;
     private boolean recording;
-    private int seconds=0;
-    private String time=" ";
+    //private int seconds=0;
+    //private String time=" ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +25,41 @@ public class sleepActivity extends AppCompatActivity {
         setTitle("Sleep");
         recordSleep = (TextView) findViewById(R.id.recordSleep);
         recording=false;
-        if(savedInstanceState!=null)
+        /*if(savedInstanceState!=null)
         {
             seconds=savedInstanceState.getInt("seconds");
             recording=savedInstanceState.getBoolean("recording");
-        }
+        }*/
         Timer();
     }
 
-    public void onSaveInstanceState(Bundle saveInstanceState)
+    /*public void onSaveInstanceState(Bundle saveInstanceState)
     {
         super.onSaveInstanceState(saveInstanceState);
         saveInstanceState.putInt("seconds", seconds);
         saveInstanceState.putBoolean("recording", recording);
-    }
+    }*/
 
     public void onClickStart(View v)
     {
         //recordSleep.setText(String.valueOf("recording"));
+        if(!recording)
+        {
+            startService(new Intent(this, SleepTimer.class));
+        }
         recording=true;
     }
 
     public void onClickStop (View v)
     {
         //recordSleep.setText(" ");
-        sleepMin=time;
+        //sleepMin=time;
+        if(recording)
+        {
+            stopService(new Intent(this, SleepTimer.class));
+        }
         recording=false;
-        seconds=0;
+        //seconds=0;
     }
 
     private void Timer()
@@ -59,17 +69,18 @@ public class sleepActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours=seconds/3600;
-                int minutes= (seconds%3600)/60;
-                int secs=seconds%60;
+                //int hours=seconds/3600;
+                //int minutes= (seconds%3600)/60;
+                //int secs=seconds%60;
 
-                time=String.format("%d:%02d:%02d", hours, minutes, secs);
+                //time=String.format("%d:%02d:%02d", hours, minutes, secs);
 
-                timeView.setText(time);
+
+                timeView.setText(sleepMin);
 
                 if(recording)
                 {
-                    seconds++;
+                    sleepMin=time;
                 }
 
                 handler.postDelayed(this, 1000);
