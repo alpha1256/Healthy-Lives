fa5f6ccbaf28be3277a766cfbeaee2089d4f4d9b
Martin Gryder <mgryd001@odu.edu>
2020-04-01 18:41:15 -0400

    added db code to logCardio

--- app/src/main/java/com/example/healthylives/logCardioActivity.java
+++ app/src/main/java/com/example/healthylives/logCardioActivity.java
@@ -2,12 +2,19 @@ package com.example.healthylives;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.healthylives.Database.DaysContract;
+
+import static com.example.healthylives.MainActivity.mHelper;
+
 public class logCardioActivity extends AppCompatActivity {
+    private String timeWorkout = new String();
     private String nameWorkout = new String();
     private String dateWorkout = new String();
     private String durationWorkout = new String();
@@ -22,11 +29,13 @@ public class logCardioActivity extends AppCompatActivity {
 
     public void onAddWork(View v)
     {
+        EditText time = findViewById(R.id.timeWork);
         EditText name = findViewById(R.id.nameWork);
         EditText date = findViewById(R.id.dateWork);
         EditText duration = findViewById(R.id.duration);
         EditText distance = findViewById(R.id.distance);
 
+        timeWorkout = time.getText().toString();
         nameWorkout = name.getText().toString();
         dateWorkout = date.getText().toString();
         durationWorkout = duration.getText().toString();
@@ -34,11 +43,23 @@ public class logCardioActivity extends AppCompatActivity {
 
         Toast.makeText(this,"Added to DB", Toast.LENGTH_SHORT).show();
 
+        time.setText("");
         name.setText("");
         date.setText("");
         duration.setText("");
         distance.setText("");
 
         //TODO add this data to database
+        SQLiteDatabase db=mHelper.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(DaysContract.DayEntry.COL_WORKOUT_TIME, timeWorkout);
+        values.put(DaysContract.DayEntry.COL_WORKOUT_NAME, nameWorkout);
+        values.put(DaysContract.DayEntry.COL_DAY_DATE, dateWorkout);
+        values.put(DaysContract.DayEntry.COL_WORKOUT_DURATION, durationWorkout);
+        values.put(DaysContract.DayEntry.COL_WORKOUT_DISTANCE, distanceWorkout);
+        db.insertWithOnConflict(DaysContract.DayEntry.TABLE2, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
+
+
     }
 }
