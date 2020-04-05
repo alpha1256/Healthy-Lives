2b34faa66803991ed482b85a423d75e16ea4c3c0
gdin <alphadeegee@gmail.com>
2020-04-05 17:50:27 -0400

    Remove unneeded code and comments

--- app/src/main/java/com/example/healthylives/Services/sendNotif.java
+++ app/src/main/java/com/example/healthylives/Services/sendNotif.java
@@ -33,17 +33,9 @@ import java.util.List;
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p>
- * TODO: Customize class - update intent actions and extra parameters.
- */
+ **/
 public class sendNotif extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    public static final String ACTION_FOO = "com.example.healthylives.Services.action.FOO";
-    public static final String ACTION_BAZ = "com.example.healthylives.Services.action.BAZ";
 
-    // TODO: Rename parameters
-    public static final String EXTRA_PARAM1 = "com.example.healthylives.Services.extra.PARAM1";
-    public static final String EXTRA_PARAM2 = "com.example.healthylives.Services.extra.PARAM2";
     private List<workoutPlan> worklist= new ArrayList<>();
 
     public sendNotif() {
@@ -102,23 +94,4 @@ public class sendNotif extends IntentService {
             //    Toast.makeText(this,"Not true", Toast.LENGTH_SHORT).show();
         }
     }
-
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }*/
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }*/
 }
