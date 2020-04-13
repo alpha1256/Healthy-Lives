cc79c6b3d5345795ecb41df9cf162694b2f1d03d
gdin <alphadeegee@gmail.com>
2020-04-13 18:19:52 -0400

    Added app bar for social activity and recyler view. Added new profile setting activity

---
+++ app/src/main/java/com/example/healthylives/Social/profileSettingActivity.java
@@ -0,0 +1,17 @@
+package com.example.healthylives.Social;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthylives.R;
+
+public class profileSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setting);
+        setTitle("Profile Settings");
+    }
+}
--- app/src/main/java/com/example/healthylives/socialActivity.java
+++ app/src/main/java/com/example/healthylives/socialActivity.java
@@ -2,7 +2,12 @@ package com.example.healthylives;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.healthylives.Social.profileSettingActivity;
 
 public class socialActivity extends AppCompatActivity {
 
@@ -11,5 +16,22 @@ public class socialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
         setTitle("Social");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.social_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.setting)
+        {
+            Intent intent = new Intent(this, profileSettingActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
