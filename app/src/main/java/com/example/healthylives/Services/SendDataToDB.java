2712ac3e985b05f7e8e40819281e8b414ea31da6
gdin <alphadeegee@gmail.com>
2020-04-27 21:36:48 -0400

    Resolve conflicts

--- app/src/main/java/com/example/healthylives/Services/SendDataToDB.java
+++ app/src/main/java/com/example/healthylives/Services/SendDataToDB.java
@@ -1,10 +1,15 @@
 package com.example.healthylives.Services;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
+import com.example.healthylives.Database.DaysContract;
 import com.example.healthylives.MainActivity;
+
+import static com.example.healthylives.MainActivity.mHelper;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -25,35 +30,6 @@ public class SendDataToDB extends IntentService {
         super("SendDataToDB");
     }
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-
-
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, SendDataToDB.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }*/
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-
-
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, SendDataToDB.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }*/
 
     @Override
     protected void onHandleIntent(Intent intent) {
@@ -78,19 +54,4 @@ public class SendDataToDB extends IntentService {
         }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-
-    private void handleActionFoo(String param1, String param2) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-
-    private void handleActionBaz(String param1, String param2) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }**/
 }
