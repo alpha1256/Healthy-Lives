cf4c0e8c95baca3376f955bf7058a4028300d5ec
gdin <alphadeegee@gmail.com>
2020-04-30 00:22:28 -0400

    Added adapter for allData recyclerview

---
+++ app/src/main/java/com/example/healthylives/Adapter/mainDataAdapter.java
@@ -0,0 +1,57 @@
+package com.example.healthylives.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.healthylives.R;
+
+import java.util.List;
+
+public class mainDataAdapter extends RecyclerView.Adapter<mainDataAdapter.Viewholder> {
+    private List <mainData> allDatalist;
+
+    public class Viewholder extends RecyclerView.ViewHolder
+    {
+        public TextView Date, activeMin, Sleep, Steps, waterCount;
+
+        public Viewholder (View v)
+        {
+            super(v);
+            Date = (TextView) v.findViewById(R.id.dateData);
+            activeMin = (TextView) v.findViewById(R.id.activeData);
+            Sleep = (TextView) v.findViewById(R.id.sleepData);
+            Steps = (TextView) v.findViewById(R.id.stepsData);
+            waterCount = (TextView) v.findViewById(R.id.waterData);
+        }
+    }
+
+    public mainDataAdapter(List <mainData> tlist){allDatalist = tlist;}
+
+    @Override
+    public Viewholder onCreateViewHolder(ViewGroup parent, int viewtype)
+    {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.data_layout,parent,false);
+        return new Viewholder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder (Viewholder holder, int position)
+    {
+        mainData temp = allDatalist.get(position);
+        holder.Date.setText(temp.getDate());
+        holder.activeMin.setText("You were active for" + temp.getActiveMin());
+        holder.Steps.setText("Steps: " + temp.getSteps());
+        holder.waterCount.setText("You had " + temp.getWaterCount() + " cups of water");
+        holder.Sleep.setText("You were asleep for "+ temp.getSleep() + " minutes");
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return allDatalist.size();
+    }
+}
---
+++ app/src/main/java/com/example/healthylives/view_AllDataActivity.java
@@ -0,0 +1,15 @@
+package com.example.healthylives;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_AllDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__all_data);
+        setTitle("View All Data");
+    }
+}
