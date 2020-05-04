b5a4b99874b26c87b91967e5f8154f9c57f88e70
Martin Gryder <mgryd001@odu.edu>
2020-05-04 15:32:32 -0400

    made a todo in friendslist activity

--- app/src/main/java/com/example/healthylives/Social/friends_listActivity.java
+++ app/src/main/java/com/example/healthylives/Social/friends_listActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.healthylives.R;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class friends_listActivity extends AppCompatActivity {
 
@@ -16,4 +19,15 @@ public class friends_listActivity extends AppCompatActivity {
     }
 
     //TODO this page should display all users and each user should be clickable. That then takes you to a page to view that users stats
+    public void updateUI()
+    {
+
+    }
+
+    //TODO write a method to get a list of users from the database
+    public ArrayList<FirebaseUser> getUsers()
+    {
+        ArrayList<FirebaseUser> users=new ArrayList<>();
+        return users;
+    }
 }
