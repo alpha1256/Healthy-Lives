bb542d41a54a9dfc5edb025416eab0a7776cac5e
Martin Gryder <mgryd001@odu.edu>
2020-05-06 19:47:19 -0400

    fixed the bug

--- app/src/main/java/com/example/healthylives/Services/WorkoutService.java
+++ app/src/main/java/com/example/healthylives/Services/WorkoutService.java
@@ -42,10 +42,10 @@ public class WorkoutService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if(intent!=null)
         {
-            WorkoutsDbHelper mHelper=new WorkoutsDbHelper(this);
+            WorkoutsDbHelper mWorkoutHelper=new WorkoutsDbHelper(this);
             List<Workout> workoutList=new ArrayList<>();
-            SQLiteDatabase db=mHelper.getReadableDatabase();
-            Cursor cursor=db.query(DaysContract.DayEntry.TABLE2, new String[]{DaysContract.DayEntry._ID, DaysContract.DayEntry.COL_WORKOUT_TIME, DaysContract.DayEntry.COL_WORKOUT_NAME, DaysContract.DayEntry.COL_DAY_DATE, DaysContract.DayEntry.COL_WORKOUT_DURATION, DaysContract.DayEntry.COL_WORKOUT_DISTANCE}, null, null, null, null, null);
+            SQLiteDatabase db=mWorkoutHelper.getReadableDatabase();
+            Cursor cursor=db.query(DaysContract.DayEntry.TABLE2, new String[]{DaysContract.DayEntry.COL_WORKOUT_TIME, DaysContract.DayEntry.COL_WORKOUT_NAME, DaysContract.DayEntry.COL_DAY_DATE, DaysContract.DayEntry.COL_WORKOUT_DURATION, DaysContract.DayEntry.COL_WORKOUT_DISTANCE}, null, null, null, null, null);
             if(cursor.getCount()<1)
             {
                 return;
